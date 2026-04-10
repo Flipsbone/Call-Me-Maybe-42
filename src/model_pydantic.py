187e5a904c95ce34827b7768606284eff580df7e
Adrien Vacher <advacher@z3r4p4.42lyon.fr>
2026-04-10 10:18:23 +0200

    refactor : config adding logic pydantic inside | add : new file function_validator to make sure all functions are given with strict validation

--- src/model_pydantic.py
+++ src/model_pydantic.py
@@ -1,33 +1,4 @@
-from pydantic import BaseModel, Field, RootModel
-from typing import Any
-from pathlib import Path
-
-
-class FunctionCalling(BaseModel):
-    prompt: str
-
-
-class ParameterModel(BaseModel):
-    type: str | None = None
-
-
-class FunctionDefinition(BaseModel):
-    name: str
-    description: str
-    parameters: dict[str, ParameterModel] = Field(default_factory=dict)
-    returns: ParameterModel
-
-
-class FunctionCallResult(BaseModel):
-    prompt: str
-    name: str
-    parameters: dict[str, Any] = Field(default_factory=dict)
-
-
-class AppConfig(BaseModel):
-    output_path: Path
-    functions_definition: list[FunctionDefinition]
-    function_calling_tests: list[FunctionCalling]
+from pydantic import RootModel
 
 
 class VocabSchema(RootModel[dict[str, int]]):
