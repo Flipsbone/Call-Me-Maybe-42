f5e6df0cf610e32fa7d65b2dc7896fe85b7f0563
flipsbone <219644104+Flipsbone@users.noreply.github.com>
2026-04-10 22:20:00 +0200

    add : some feature color, time | add : exit keyboard prog is now handle , json save properly

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import time
 
 from src.config import setup_configuration
 from src.functions_validator import FunctionCallResult
@@ -8,9 +9,19 @@ from src.generator import ConstrainedGenerator
 from src.json_generator import process_single_prompt_optimized
 from src.state_machine import JsonStateMachine, StateTerminal
 from llm_sdk import Small_LLM_Model
+from pydantic import BaseModel, Field
+
+
+class TerminalColor(BaseModel):
+    GREEN: str = Field(default="\033[92m", frozen=True)
+    RED: str = Field(default="\033[91m", frozen=True)
+    CYAN: str = Field(default="\033[96m", frozen=True)
+    RESET: str = Field(default="\033[0m", frozen=True)
 
 
 def main() -> None:
+    start_time = time.time()
+    theme = TerminalColor()
     config = setup_configuration()
     print("Initializing the LLM model and vocabulary...")
 
@@ -30,23 +41,30 @@ def main() -> None:
     )
 
     results = []
+    try:
+        for test_case in config.function_calling_tests:
+            print(f"{theme.CYAN}Processing: '{test_case.prompt}'"
+                  f"...{theme.RESET}")
+            try:
+                result_dict = process_single_prompt_optimized(
+                    test_case.prompt,
+                    config.functions_definition,
+                    generator
+                )
+                validated_result = FunctionCallResult.model_validate(
+                    result_dict)
+                results.append(validated_result.model_dump())
+                print(f"  {theme.GREEN}✓ Success:{theme.RESET}"
+                      f"{result_dict.get('name', 'Unknown')}")
 
-    for test_case in config.function_calling_tests:
-        print(f"Processing: '{test_case.prompt}'...")
-        try:
-            result_dict = process_single_prompt_optimized(
-                test_case.prompt,
-                config.functions_definition,
-                generator
-            )
-            validated_result = FunctionCallResult.model_validate(
-                result_dict)
-            results.append(validated_result.model_dump())
-            print(f"  ✓ Success: {result_dict.get('name', 'Unknown')}")
-
-        except Exception as e:
-            print(f"  ✗ Error on prompt '{test_case.prompt}': {e}")
-            continue
+            except Exception as e:
+                print(f"  {theme.RED}✗ Error prompt '{test_case.prompt}':"
+                      f"{e}.{theme.RESET}")
+                continue
+    except KeyboardInterrupt:
+        print(f"\n{theme.RED}✗ User interrupted (Ctrl+C)."
+              f"Shutting down...{theme.RESET}")
+        print("Saving processed results...")
 
         if results:
             try:
@@ -56,7 +74,9 @@ def main() -> None:
                 sys.exit(f"  ✗ Error saving the JSON file: {e}")
         else:
             print("\n No results were generated. File not saved.")
-
+    elapsed_time = time.time() - start_time
+    print(f"\n{theme.CYAN}Total execution time:"
+          f"{theme.GREEN}{elapsed_time:.2f}{theme.CYAN} seconds.{theme.RESET}")
     print(f"\n✓ All results successfully saved to {config.output_path}")
 
 
